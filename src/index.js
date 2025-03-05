815e74e3a9a39cbbc60fb66b3b0c771a29a7c8aa
JADHAV ARYAN <jadhavaryan906@gmail.com>
2025-03-05 11:59:15 +0530

    connect to mango atlas

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+// import express from "express"
+// const app=express()
+// (async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//        app.on("error",()=>{
+//         console.log("ERROR",error)
+//         throw err
+//        })
+
+//        app .listen(process.env.PORT,()=>{
+//         console.log(`App is listing on ${process.env.PORT}`)
+//        })
+//     } catch (error) {
+//         console.log("ERROR",error)
+//         throw err
+//     }
+// })()
