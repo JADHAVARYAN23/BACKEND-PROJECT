2764a57f0ac3ea55047fc09a68e45ef144154283
JADHAV ARYAN <jadhavaryan906@gmail.com>
2025-03-06 11:49:53 +0530

    add user and video models

---
+++ src/models/user.model.js
@@ -0,0 +1,80 @@
+import mongoose,{Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+const userScema=new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String,
+        required:true,
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,'Passwd is required']
+    },
+    refreshToken:{
+        type:String
+    }
+},{timestamps:true})
+
+userScema.pre("save",async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+userScema.methods.isPasswaordCorrect=async function (password){
+   return await bcrypt.compare(password,this.password)
+}
+userScema.methods.generateAccessToken=function(){
+   return  jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userScema.methods.generateAccessRefreshToken=function(){
+    return  jwt.sign(
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,{
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+export const User=mongoose.model("User",userScema)
